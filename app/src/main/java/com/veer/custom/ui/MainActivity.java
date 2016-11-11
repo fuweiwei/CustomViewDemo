1293aa9693821211b03e9761a48cd8a5d47495fc
fuweiwei <fuweiwei@kjtpay.com.cn>
2016-11-11 10:49:35 +0800

    优化

--- app/src/main/java/com/veer/custom/ui/MainActivity.java
+++ app/src/main/java/com/veer/custom/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.veer.custom.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
         button5.setOnClickListener(this);
+//        setTitle("测试");
     }
 
     @Override
